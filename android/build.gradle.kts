dac3aeb09f23380b1f2bc4cfa3ef6e0fc1d64c58
jyodesh10 <jyodeshshakya@gmail.com>
2025-10-04 02:32:28 +0200

    modified build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -9,12 +9,48 @@ val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build"
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    // fix for verifyReleaseResources
+    // ============
+    afterEvaluate {
+        val project = this // 'this' refers to the Project
+
+        if (project.plugins.hasPlugin("com.android.application") || project.plugins.hasPlugin("com.android.library")) {
+            val androidExtension =
+                project.extensions.getByName("android") as? com.android.build.gradle.LibraryExtension
+                    ?: project.extensions.getByName("android") as? com.android.build.gradle.AppExtension
+
+            androidExtension?.apply {
+                compileSdkVersion(35)
+                buildToolsVersion = "35.0.0"
+            }
+        }
+
+        if (project.extensions.findByName("android") != null) {
+            val androidExtension =
+                project.extensions.getByName("android") as? com.android.build.gradle.LibraryExtension
+                    ?: project.extensions.getByName("android") as? com.android.build.gradle.AppExtension
+
+            androidExtension?.apply {
+                if (namespace == null) {
+                    namespace = project.group.toString()
+                }
+            }
+        }
+    }
+    // ============
+    buildDir = file("${rootProject.buildDir}/${project.name}")
+    evaluationDependsOn(":app")
 }
-subprojects {
-    project.evaluationDependsOn(":app")
-}
+
+
+// subprojects {
+//     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+//     project.layout.buildDirectory.value(newSubprojectBuildDir)
+// }
+
+// subprojects {
+//     project.evaluationDependsOn(":app")
+// }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
